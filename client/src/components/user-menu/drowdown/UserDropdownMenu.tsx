516e4dbd56c559a50ad31ab565056bea5c2cfc92
Taliah Wharton <taliahwh23@yahoo.com>
2023-05-24 12:27:32 -0400

    close menu after navigation

--- client/src/components/user-menu/drowdown/UserDropdownMenu.tsx
+++ client/src/components/user-menu/drowdown/UserDropdownMenu.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 // Assets
 import { BsFillPenFill, BsFillBookmarkHeartFill } from 'react-icons/bs';
@@ -9,34 +9,62 @@ import { HiUser } from 'react-icons/hi';
 // Styles
 import styles from './dropdown.module.scss';
 
-const UserDropdownMenu = () => {
+type UserDropdownMenuProps = {
+  closeMenu: () => void;
+};
+
+const UserDropdownMenu = ({ closeMenu }: UserDropdownMenuProps) => {
+  const navigate = useNavigate();
+
+  const navigateTo = (route: string, id: string) => {
+    navigate(`${route}/${id}`);
+    closeMenu();
+  };
+
   return (
     <div className={styles.dropdownContainer}>
       <div className={styles.topMenu}>
         <div className={styles.menuItem}>
           <BsFillPenFill className={styles.icon} />
-          <div className={styles.title}>Create a Post</div>
+          <div
+            className={styles.title}
+            onClick={() => navigateTo('/create-post', '1')}
+          >
+            Create a Post
+          </div>
         </div>
 
         <div className={styles.menuItem}>
           <HiUser className={styles.icon} />
-          <Link to={`/profile/:id`} className={styles.link}>
-            <div className={styles.title}>Profile</div>
-          </Link>
+
+          <div
+            className={styles.title}
+            onClick={() => navigateTo('/profile', '1')}
+          >
+            Profile
+          </div>
         </div>
 
         <div className={styles.menuItem}>
           <BsFillBookmarkHeartFill className={styles.icon} />
-          <Link to={`/settings/profile/:id/bookmarks`} className={styles.link}>
-            <div className={styles.title}>Bookmarks</div>
-          </Link>
+
+          <div
+            className={styles.title}
+            onClick={() => navigateTo('/settings/profile/bookmarks', '1')}
+          >
+            Bookmarks
+          </div>
         </div>
 
         <div className={styles.menuItem}>
           <IoMdSettings className={styles.icon} />
-          <Link to={`/settings/profile/:id`} className={styles.link}>
-            <div className={styles.title}>Settings</div>
-          </Link>
+
+          <div
+            className={styles.title}
+            onClick={() => navigateTo('/settings/profile', '1')}
+          >
+            Settings
+          </div>
         </div>
       </div>
 
